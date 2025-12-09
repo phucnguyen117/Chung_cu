6a03bedd78bb08eebffcd8605b65f3c27d314c96
PhucNguyen <hoangnguyenkiok@gmail.com>
2025-12-09 15:00:37 +0700

    test16

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -38,7 +38,7 @@ export default function Login({ onClose, onSwitchToRegister }) {
     try {
       setLoading(true)
         const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
-        const res = await fetch(`${API_URL}/api/login`, {
+        const res = await fetch(`${API_URL}/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
